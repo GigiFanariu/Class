a72e1c1e933b488968a95d74da69d8d0e2cc6482
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-08-03 23:19:49 +0200

    risolto errore

--- src/TestProgrammers.java
+++ src/TestProgrammers.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class TestProgrammers {
+public class TestProgrammers {
+    public static void main(String[] args) {
+
+        Programmer programmer1 = new Programmer();
+        programmer1.name = "Gigi";
+        programmer1.age = 22;
+        programmer1.wearsGlasses = false;
+
+        programmer1.drinkCoffee();
+        programmer1.printDetails();
+
+
+        Programmer programmer2 = new Programmer();
+        programmer2.name = "Jonh";
+        programmer2.age = 25;
+        programmer2.wearsGlasses = true;
+
+        programmer2.drinkCoffee();
+        programmer2.hasGlasses();
+
+
+
+    }
 }
